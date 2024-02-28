698ec64c94ad60d1ceb06c690327c48bf616086f
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-02-28 18:02:37 +0600

    create a json data for to category

--- src/components/TopCategories.jsx
+++ src/components/TopCategories.jsx
@@ -1,13 +1,52 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 const TopCategories = () => {
-    return (
-        <section className="w-11/12 mx-auto mb-10">
-            <div className="gap-10 justify-around bg-white shadow-md relative -top-[40px] text-center p-5">
-           <h1>tarek</h1>
-           <h1>tarek</h1>
-           <h1>tarek</h1>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+  };
+  return (
+    <section className="w-11/12 mx-auto mb-10">
+      <div className="bg-white shadow-md relative -top-[40px] text-center p-5">
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
             </div>
-        </section>
-    );
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+            <div>
+              <h3>9</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TopCategories;
